61506857ec4dc364a2ddcc4f6c0af09391dc8c7c
GUIDEV19 <guilherme.bezerradoctor@gmail.com>
2025-06-08 18:31:28 -0300

    <FEAT> Rota Status.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('status')
+  getStatus(): { status: number, version: string } {
+    return {
+      version: process.env.npm_package_version || '0.0.0',
+      status: HttpStatus.OK
+    };
   }
 }
