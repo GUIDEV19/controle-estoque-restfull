5439d83512d581e3059b23cb163bc69cb282482a
GUIDEV19 <guilherme.bezerradoctor@gmail.com>
2025-06-09 21:45:22 -0300

    <FEAT> Implementando Loogers com winton

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { databaseProviders } from './config/database.provider';
 import { EntityModule } from './entitys/entity.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CommonModule,
     EntityModule,
   ],
   controllers: [],
