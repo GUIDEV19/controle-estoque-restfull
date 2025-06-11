a8a5067c8cf22f37f9444efbe64b7d07c3d26738
GUIDEV19 <guilherme.bezerradoctor@gmail.com>
2025-06-10 21:55:37 -0300

    <FEAT> Adicionando validações no salvamento.

--- src/app/entitys/bussines/entity.service.ts
+++ src/app/entitys/bussines/entity.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException, InternalServerErrorException, BadRequestException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { TbEntity } from '../domain/entity.entity';
 import { CustomLoggerService } from '../../../common/services/logger.service';
@@ -10,17 +10,17 @@ export class EntityService {
     @Inject('ENTITY_REPOSITORY')
     private entityRepository: Repository<TbEntity>,
     private readonly logger: CustomLoggerService,
-  ) {}
+  ) { }
 
   async findAll(): Promise<TbEntity[]> {
-    try{
+    try {
       const entities = await this.entityRepository.find();
-      if(entities.length === 0){
+      if (entities.length === 0) {
         throw new NotFoundException('No se encontraron entidades');
       }
       return entities;
-    }catch(error){
-      if(error instanceof NotFoundException){
+    } catch (error) {
+      if (error instanceof NotFoundException) {
         this.logger.error('Entity not found error', error.stack, 'EntityService');
         throw error;
       }
@@ -29,11 +29,28 @@ export class EntityService {
     }
   }
 
+  async findByDocument(document: string): Promise<TbEntity | any> {
+    try {
+      const entity = await this.entityRepository.findOne({ where: { document } });
+      return entity;
+    } catch (error) {
+      this.logger.error('Error fetching entity by document', error.stack, 'EntityService');
+    }
+  }
+
   async save(entity: EntityDto): Promise<TbEntity> {
-    try{
+    try {
+      const entityExists = await this.findByDocument(entity.document);
+      if (entityExists) {
+        this.logger.error('Entity already exists', 'EntityService');
+        throw new BadRequestException('Entity already exists');
+      }
       const newEntity = await this.entityRepository.save(entity);
       return newEntity;
-    }catch(error){
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
       this.logger.error('Error saving entity', error.stack, 'EntityService');
       throw new InternalServerErrorException('Error al guardar la entidad');
     }
