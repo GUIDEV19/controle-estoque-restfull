24dacbcb6f2b61f75d5ab853a6a4dfb079314592
GUIDEV19 <guilherme.bezerradoctor@gmail.com>
2025-06-22 11:31:45 -0300

    <FEAT> Criando restante de rotas para manipulação de produtos.

--- src/app/products/bussines/products.service.ts
+++ src/app/products/bussines/products.service.ts
@@ -1,7 +1,7 @@
-import { BadRequestException, Inject, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { BadRequestException, Inject, Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { Product } from "../domain/products.entity";
 import { Repository } from "typeorm";
-import { CreateProductDto } from "../dto/products.dto";
+import { CreateProductDto, UpdateProductDto } from "../dto/products.dto";
 import { CategoriesService } from "src/app/categories/busssines/categories.service";
 import { CustomLoggerService } from "src/common/services/logger.service";
 import { AuthenticatedUserDto } from "src/app/users/dto/user.dto";
@@ -39,6 +39,77 @@ export class ProductsService {
             }
             this.logger.error('Error creating product', error.stack, 'ProductsService');
             throw new InternalServerErrorException('Error creating product');
+        }
+    }
+
+    async findAll(): Promise<Product[]> {
+        try {
+            const products = await this.productRepository.find();
+            if (!products) {
+                throw new NotFoundException('Products not found');
+            }
+            return products;
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                this.logger.warn('Products not found', 'ProductsService');
+                throw error;
+            }
+            this.logger.error('Error fetching products', error.stack, 'ProductsService');
+            throw new InternalServerErrorException('Error fetching products');
+        }
+    }
+
+    async findOne(id: number): Promise<Product> {
+        try {
+            const product = await this.productRepository.findOne({ where: { id } });
+            if (!product) {
+                throw new NotFoundException('Product not found');
+            }
+            return product;
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                this.logger.warn('Product not found', 'ProductsService');
+                throw error;
+            }
+            this.logger.error('Error fetching product', error.stack, 'ProductsService');
+            throw new InternalServerErrorException('Error fetching product');
+        }
+    }
+
+    async update(id: number, updateProductDto: UpdateProductDto, user: AuthenticatedUserDto): Promise<Product> {
+        try {
+            const product = await this.productRepository.findOne({ where: { id } });
+            if (!product) {
+                throw new NotFoundException('Product not found');
+            }
+            return await this.productRepository.save({id, ...updateProductDto, user_updated_id: user.id });
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                this.logger.warn('Product not found', 'ProductsService');
+                throw error;
+            }
+            this.logger.error('Error updating product', error.stack, 'ProductsService');
+            throw new InternalServerErrorException('Error updating product');
+        }
+    }
+
+    async delete(id: number, user: AuthenticatedUserDto): Promise<void> {
+        try {
+            const product: Product | any = await this.productRepository.findOne({ where: { id } });
+            if (!product) {
+                throw new NotFoundException('Product not found');
+            }
+            product.user_updated_id = user.id; 
+            await this.productRepository.save(product);
+            
+            await this.productRepository.softDelete(id);
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                this.logger.warn('Product not found', 'ProductsService');
+                throw error;
+            }
+            this.logger.error('Error deleting product', error.stack, 'ProductsService');
+            throw new InternalServerErrorException('Error deleting product');
         }
     }
 }
--- src/app/products/controller/products.controller.ts
+++ src/app/products/controller/products.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Post, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
 import { ProductsService } from "../bussines/products.service";
-import { CreateProductDto } from "../dto/products.dto";
+import { CreateProductDto, UpdateProductDto } from "../dto/products.dto";
 import { AuthenticatedUserDto } from "src/app/users/dto/user.dto";
 import { CurrentUser } from "../../auth/decorators/current-user.decorator";
 import { JwtAuthGuard } from "../../auth/guards/jwt-auth.guard";
-import { UserContextInterceptor } from "../../auth/interceptors/user-context.interceptor";
 
 @Controller('products')
 @UseGuards(JwtAuthGuard)
-@UseInterceptors(UserContextInterceptor)
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
 
@@ -18,5 +16,34 @@ export class ProductsController {
         @CurrentUser() user: AuthenticatedUserDto
     ) {
         return this.productsService.create(createProductDto, user);
+    }
+
+    @Get()
+    async findAll() {
+        return this.productsService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(
+        @Param('id') id: number
+    ) {
+        return this.productsService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id: number,
+        @Body() updateProductDto: UpdateProductDto,
+        @CurrentUser() user: AuthenticatedUserDto
+    ) {
+        return this.productsService.update(id, updateProductDto, user);
+    }
+
+    @Delete(':id')
+    async delete(
+        @Param('id') id: number,
+        @CurrentUser() user: AuthenticatedUserDto
+    ) {
+        return this.productsService.delete(id, user);
     }
 }
